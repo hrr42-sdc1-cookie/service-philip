6ffcc0384e20b2ae587b6204a8592a9b99310c41
Philip Enchin <Philip@Ench.in>
2019-12-18 00:00:57 -0500

    Lint Reservation.jsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   extends: 'airbnb',
   rules: {
     "arrow-parens": ["error", "as-needed"],
-    "import/no-extraneous-dependencies": ["error", {"devDependencies": true}]
+    "import/no-extraneous-dependencies": ["error", {"devDependencies": true}],
+    "no-plusplus": "off",
   },
   env: {
     browser: true,
