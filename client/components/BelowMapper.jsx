94ca8869c33606e6e8d239c9e4e22b5e78be039f
Philip Enchin <Philip@Ench.in>
2019-12-17 22:52:12 -0500

    Lint BelowMapper.jsx

--- client/components/BelowMapper.jsx
+++ client/components/BelowMapper.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-
-var BelowMapper = () => (
+const BelowMapper = () => (
   <div className="below-mapper">
     <div className="streetBox">
-      <img style={{marginTop: '5px', marginBottom: '-8px'}} src="assets/ic_location.png"></img>
+      <img style={{ marginTop: '5px', marginBottom: '-8px' }} src="assets/ic_location.png" alt="map pin" />
       &nbsp;1313 Mockingbird Lane, San Francisco, CA
     </div>
-    <img style={{marginLeft: '-10px'}} src="assets/otRightBar.png"></img>
+    <img style={{ marginLeft: '-10px' }} src="assets/otRightBar.png" alt="placeholder for restaurant information" />
   </div>
 );
 
