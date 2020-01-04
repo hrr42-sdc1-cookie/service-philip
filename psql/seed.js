0aa1bef77aee7499f15f719f429a128f93b91990
Philip Enchin <Philip@Ench.in>
2020-01-04 16:16:55 -0500

    Add IF EXISTS clause to foreign key drop and add time to cli feedback

--- psql/seed.js
+++ psql/seed.js
@@ -5,7 +5,7 @@ const restaurantCsvPath = path.resolve(__dirname, '../dataGeneration/restaurants
 const reservationCsvPath = path.resolve(__dirname, '../dataGeneration/reservations100k.csv');
 
 const dropForeignKey = `
-  ALTER TABLE reservations DROP CONSTRAINT reservations_restaurantid_fkey;
+  ALTER TABLE reservations DROP CONSTRAINT IF EXISTS reservations_restaurantid_fkey;
 `;
 const restaurantCopy = `
   COPY restaurants(id,name,seats,tables,latitude,longitude)
@@ -21,30 +21,32 @@ const replaceForeignKey = `
   FOREIGN KEY (restaurantid) REFERENCES restaurants (id);
 `;
 
+const time = () => (new Date()).toLocaleTimeString();
+
 let d = Date.now();
 schemaPromise
   .then(() => {
-    console.log('Removing foreign key from reservations');
+    console.log(time(), 'Removing foreign key from reservations');
     return client.query(dropForeignKey);
   })
   .then(() => {
-    console.log('Seeding...');
+    console.log(time(), 'Seeding...');
     return client.query(restaurantCopy);
   })
   .then(({ rowCount }) => {
-    console.log(`Restaurants populated in ${(-d + (d = Date.now())) / 1000} seconds...`);
+    console.log(time(), `Restaurants populated in ${(-d + (d = Date.now())) / 1000} seconds...`);
     return client.query(restaurantSetSeq, [rowCount]);
   })
   .then(() => {
-    console.log('Restaurant pimary key is set...');
+    console.log(time(), 'Restaurant pimary key is set...');
     return client.query(reservationCopy);
   })
   .then(() => {
-    console.log(`Reservations populated in ${(-d + (d = Date.now())) / 1000} seconds...`);
+    console.log(time(), `Reservations populated in ${(-d + (d = Date.now())) / 1000} seconds...`);
     return client.query(replaceForeignKey);
   })
   .then(() => {
-    console.log(`Foreign key for reservations replaced in ${(-d + (d = Date.now())) / 1000} seconds`);
+    console.log(time(), `Foreign key for reservations replaced in ${(-d + (d = Date.now())) / 1000} seconds`);
     console.log('Seeded database is ready to rock.');
   })
   .catch(err => console.log(err))
