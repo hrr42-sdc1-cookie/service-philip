2251ff246740e9995a728cad740d63e2b9f529c2
Philip Enchin <Philip@Ench.in>
2020-01-09 23:08:57 -0500

    Send a notification message to server after attempting a reservation insert

--- psql/dbFunctions.js
+++ psql/dbFunctions.js
@@ -1,16 +1,36 @@
 const client = require('./connectClient');
 
-const createReservation = ({
-  restId, name, time, guests,
-}) => {
-  const query = 'INSERT INTO reservations '
+const createReservationInsert = (restId, name, time, guests) => {
+  const insert = 'INSERT INTO reservations '
   + '(restaurantId, customerName, time, guestCount) '
   + 'VALUES '
   + '($1, $2, $3, $4)'
   + 'RETURNING *';
-  const values = [restId, name, time, guests];
-  return client.query(query, values)
+  const values = [restId, name, new Date(time), guests];
+  return client.query(insert, values)
     .then(({ rows }) => rows[0]);
+};
+
+const createReservation = ({
+  restId, name, time, guests,
+}) => {
+  const utcTime = new Date(time);
+  const before = new Date(time);
+  const after = new Date(time);
+  before.setHours(before.getHours() - 2);
+  after.setHours(after.getHours() + 2);
+
+  const select = 'SELECT * FROM reservations WHERE restaurantId = $1 AND customerName = $2 AND time > $3 AND time < $4';
+  const selectValues = [restId, name, before, after];
+
+  return client.query(select, selectValues)
+    .then(({ rows }) => {
+      if (rows.length) {
+        return 'Sorry, you already have a booking at this time.  Please make another selection.';
+      }
+      return createReservationInsert(restId, name, utcTime, guests)
+        .then(() => 'Your reservation has been successfully booked!');
+    });
 };
 
 module.exports = {
